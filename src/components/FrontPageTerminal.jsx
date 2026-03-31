e2f6e3fde8badb38bb1f30d5069c10baa6e2f343
AyaanSharma2007 <ayaan007sharma@gmail.com>
2026-03-31 16:57:01 +0530

    added front page terminal component

---
+++ src/components/FrontPageTerminal.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
+
+export default function FrontPageTerminal() {
+  // Create reference to store the DOM element containing the animation
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+      typeSpeed: 50,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <span ref={el} />
+    </div>
+  );
+}
