a6b72410abd77296d32d6c1f6480b4b679075a1f
AyaanSharma2007 <ayaan007sharma@gmail.com>
2026-04-01 19:39:50 +0530

    add page flip

--- src/components/Newspaper.jsx
+++ src/components/Newspaper.jsx
@@ -1,24 +1,61 @@
 import "../styles/Newspaper.css";
+import React from "react";
 import HTMLFlipBook from "react-pageflip";
 export default function Newspaper() {
+
+  const Page = React.forwardRef((props, ref) => {
+    return (
+        <div className="demoPage" ref={ref}>
+            {/* <h1>Page Header</h1>
+            <p>{props.children}</p>
+            <p>Page number: {props.number}</p> */}
+            <SinglePage />
+        </div>
+    );
+});
+// function MyBook(props) {
+//     return (
+//         <HTMLFlipBook width={300} height={500}>
+//             <Page number="1">Page text</Page>
+//             <Page number="2">Page text</Page>
+//             <Page number="3">Page text</Page>
+//             <Page number="4">Page text</Page>
+//         </HTMLFlipBook>
+//     );
+// }
+// function MyBook(props) {
+//     return (
+//         <HTMLFlipBook width={300} height={500}>
+//             <Page number="1">Page text</Page>
+//             <Page number="2">Page text</Page>
+//             <Page number="3">Page text</Page>
+//             <Page number="4">Page text</Page>
+//         </HTMLFlipBook>
+//     );
+// }
+
   function MyBook(props) {
     return (
       <HTMLFlipBook
         width={window.innerWidth * 0.3}
         height={window.innerHeight * 0.8}
       >
-        <div className="demoPage bg-[url('/assets/Newspaper.png')] bg-cover">
+        <Page className="demoPage bg-[url('/assets/Newspaper.png')] bg-cover">
+          <singlePage className="demoPage" />
           Page 1
-        </div>
-        <div className="demoPage bg-[url('/assets/Newspaper.png')] bg-cover">
+        </Page>
+        <Page className="demoPage bg-[url('/assets/Newspaper.png')] bg-cover">
+          <singlePage className="demoPage" />
           Page 2
-        </div>
-        <div className="demoPage bg-[url('/assets/Newspaper.png')] bg-cover">
+        </Page>
+        <Page className="demoPage bg-[url('/assets/Newspaper.png')] bg-cover">
+          <singlePage className="demoPage" />
           Page 3
-        </div>
-        <div className="demoPage bg-[url('/assets/Newspaper.png')] bg-cover">
+        </Page>
+        <Page className="demoPage bg-[url('/assets/Newspaper.png')] bg-cover">
+          <singlePage className="demoPage" />
           Page 4
-        </div>
+        </Page>
       </HTMLFlipBook>
     );
   }
@@ -26,6 +63,21 @@ export default function Newspaper() {
   return (
     <>
       <div className="bgTable bg-[url('/assets/table.png')] w-screen h-screen flex justify-center items-center bg-cover overflow-hidden">
+        <MyBook />
+        {/* <div className="main bg-[url('/assets/Newspaper.png')] w-[80vw] bg-black h-[80vh] bg-contain repeat">
+          5
+          <h1 className="newspaperTitle text-7xl font-[cursive] text-center  text-gray-800">
+            Linked Out
+          </h1>
+          <MyBook/>
+        </div> */}
+      </div>
+    </>
+  );
+}
+function SinglePage() {
+  return (
+    <>
         {/* <MyBook  /> */}
         {/* <div className="main bg-[url('/assets/Newspaper.png')] w-[80vw] bg-black h-[80vh] bg-contain repeat">
           5
@@ -39,29 +91,42 @@ export default function Newspaper() {
           <div className="newsContent flex gap-6 mt-2">
             <div className="col1 ">
               <h2 className="newsHeading text-2xl text-center">Falana 1</h2>
-              <img src="/assets/test.png" className="newsImage max-w-[55%] float-left mr-4 " />
+              <img
+                src="/assets/test.png"
+                className="newsImage max-w-[55%] float-left mr-4 "
+              />
               <p className="newsContent text-justify">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
                 numquam quaerat ipsa commodi earum optio quia ut, quis
                 distinctio pariatur cumque amet vitae dolore non nobis. Atque
                 numquam nihil facere vitae quis aliquid incidunt quia, voluptas
-                sequi. Nam, at maiores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque blanditiis omnis doloribus quidem. Fugiat obcaecati veritatis ea nihil! Eveniet similique inventore at, tempora numquam delectus quidem pariatur recusandae odio rem?
+                sequi. Nam, at maiores! Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Itaque blanditiis omnis doloribus quidem.
+                Fugiat obcaecati veritatis ea nihil! Eveniet similique inventore
+                at, tempora numquam delectus quidem pariatur recusandae odio
+                rem?
               </p>
             </div>
             <div className="col2 ">
               <h2 className="newsHeading text-2xl text-center">Falana 2</h2>
-              <img src={"/assets/test.png"} className="newsImage max-w-[55%] float-left mr-4" />
+              <img
+                src={"/assets/test.png"}
+                className="newsImage max-w-[55%] float-left mr-4"
+              />
               <p className="newsContent text-justify">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
                 numquam quaerat ipsa commodi earum optio quia ut, quis
                 distinctio pariatur cumque amet vitae dolore non nobis. Atque
                 numquam nihil facere vitae quis aliquid incidunt quia, voluptas
-                sequi. Nam, at maiores! Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque blanditiis omnis doloribus quidem. Fugiat obcaecati veritatis ea nihil! Eveniet similique inventore at, tempora numquam delectus quidem pariatur recusandae odio rem?
+                sequi. Nam, at maiores! Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Itaque blanditiis omnis doloribus quidem.
+                Fugiat obcaecati veritatis ea nihil! Eveniet similique inventore
+                at, tempora numquam delectus quidem pariatur recusandae odio
+                rem?
               </p>
             </div>
           </div>
         </div>
-      </div>
     </>
   );
 }
